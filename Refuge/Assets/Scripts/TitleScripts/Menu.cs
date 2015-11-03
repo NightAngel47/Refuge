c9a896f59d7a3943563e76d12c4a430bf103859e
NightAngel47 <sjdrovie@gmail.com>
2015-11-03 16:26:31 -0600

    Prototype 0.0.1
    Got a title screen working with scenes, title and game.
    
    Added:
    -Title "Refuge"
    -Start button
    -Quit button
    -Script "Menu", handles start and quit buttons.
    -build of "Prototype 0.0.1" for scripting purposes
    
    Goals:
    -create a way to get back to "title" from "game"
    -more advance menu
    -prototype level (would like to have some art for this)

---
+++ Refuge/Assets/Scripts/TitleScripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public void Awake(){
+	
+	}
+
+	public void StartButtonClicked (){
+	
+		Application.LoadLevel (1);
+		Debug.Log ("Game started.");
+	}
+	
+	public void QuitButtonClicked (){
+		//If we are running in a standalone build of the game
+		#if UNITY_STANDALONE
+		//Quit the application
+		Application.Quit ();
+		Debug.Log ("Game quit");
+		#endif
+		
+		//If we are running in the editor
+		#if UNITY_EDITOR
+		//Stop playing the scene
+		UnityEditor.EditorApplication.isPlaying = false;
+		Debug.Log ("Game quit");
+		#endif
+	}
+}
